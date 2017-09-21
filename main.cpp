af3467d14c58cf8f73c65217d252d09a6f4b60f9
Pavel Zharow <trother555@gmail.com>
2017-09-21 20:21:52 +0300

    Added base ui

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QStyleFactory>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
+    qApp->setStyleSheet("QGroupBox {\
+                        border: 0;\
+                        padding-left: -8;\
+                        padding-right: -8;\
+                        padding-bottom: -6;\
+                        padding-top: -10;\
+                    }");
     MainWindow w;
     w.show();
-
     return a.exec();
 }
